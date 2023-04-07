c35efe555c3d171fc5d5b7f9c9ee59bf8586ac97
Smart-SpartacuS <pokevhee@gmail.com>
2023-04-07 15:19:07 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  base: "/spk-saw/",
+});
